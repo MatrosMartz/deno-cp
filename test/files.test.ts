27532f929223cd57d8f990e5a744c9221521a0b8
TsonkoAlt <matrosmartz@gmal.com>
2022-07-25 00:56:22 -0500

    files test

---
+++ test/files.test.ts
@@ -0,0 +1,28 @@
+import { assertStrictEquals, assert } from 'testing/asserts.ts'
+
+import { cmd } from '../utils/tests.ts'
+
+Deno.test('copy example.txt to example2.txt', async t => {
+  const p = Deno.run({
+    cmd: cmd('./res/example.txt ./res/example2.txt')
+  })
+
+  p.close()
+
+  await t.step('example2.txt create', async () => {
+    const actualText = await Deno.readTextFile('./res/example2.txt')
+    
+    assert(actualText)
+  })
+  
+  await t.step('example2.txt are equal to example.txt', async () => {
+    const actualText = await Deno.readTextFile('./res/example2.txt')
+    
+    const expectedText = await Deno.readTextFile('./res/example.txt')
+
+    assertStrictEquals(
+      actualText,
+      expectedText
+    )
+  })
+})
