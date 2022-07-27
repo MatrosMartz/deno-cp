82d8d05e69caa71a5dd582c2b056919d7dff7ae7
TsonkoAlt <matrosmartz@gmal.com>
2022-07-27 02:04:05 -0500

    refactor expected-errors NotSuch :D

--- test-data/expected-errors.ts
+++ test-data/expected-errors.ts
@@ -4,7 +4,7 @@ export default <ExpectedErrors> {
   err: {
     MissingFiles: 'cp: missing file operand\nTry \'cp --help\' for more information.\n',
     NotADictory: 'cp: cannot create regular file \'./res/no-exist-dir/\': Not a directory',
-    NoSuch: 'cp: cannot stat \'./res/no-exist-file\': No such file or directory',
+    NoSuch: 'cp: cannot stat \'./res/no-exist\': No such file or directory',
   },
   status: {
     code: 1,
