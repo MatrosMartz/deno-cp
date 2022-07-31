ee02f5d6a092659cf3a8238272055a89d0bef29a
TsonkoAlt <matrosmartz@gmal.com>
2022-07-31 16:02:45 -0500

    add test for copy inside directory renaming it

--- test/files.test.ts
+++ test/files.test.ts
@@ -82,6 +82,45 @@ Deno.test('copy one file in to directory dest', async t => {
   })
 })
 
+Deno.test('copy one file in to directory dest renaming id', async t => {
+  const process = Deno.run({
+    cmd: cmd(['./res/copy-file-1.txt', './res/dest-dir/created-file'])
+  })
+
+  process.close()
+
+  await t.step('correct execution', async () => {
+    const actualStatus = await process.status() 
+    assertEquals(
+      actualStatus,
+      expectedStatus
+    )
+  })
+
+  await t.step('new file is file', async () => {
+    const actualExampleInfo = await Deno.lstat('./res/dest-dir/created-file.txt')
+
+    assert(actualExampleInfo.isFile)
+  })
+  
+  await t.step('new file content is equal to original content', async () => {
+    const actualText = await Deno.readTextFile('./res/dest-dir/created-file.txt')
+    
+    const expectedText = await Deno.readTextFile('./res/copy-file-1.txt')
+
+    assertStrictEquals(
+      actualText,
+      expectedText
+    )
+  })
+
+  await t.step('deleted new file', async () => {
+    assert(
+      await Deno.remove('./res/dest-dir/created-file.txt')
+    )
+  })
+})
+
 Deno.test('copy multiple files in to directory dest', async t => {
   const process = Deno.run({
     cmd: cmd(['copy-file-1', 'copy-file-2', 'dest-dir'])
