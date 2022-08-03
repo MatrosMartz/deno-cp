5ad150d5ec105adac4b1b65bd89988c571500223
TsonkoAlt <matrosmartz@gmal.com>
2022-08-03 15:53:35 -0500

    refactor not found error and add not a directory error

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { join } from 'path'
+import { join, basename } from 'path'
 
 import { CPErrors } from '../types/enums.ts'
 
-import { getFileError } from '../utils/errors.ts'
+import { exists, srcsExist } from '../utils/errors.ts'
 
-import { showInfo, showVersion, showError } from './show.ts'
+import { showInfo, showVersion, showError } from '../utils/show.ts'
 
 const firstArg = Deno.args[0]
 
@@ -14,53 +14,37 @@ const version = ['--version', '-V'].includes(firstArg)
 if (!Deno.args.length) showError(CPErrors.MissingFiles)
 else if (help) showInfo()
 else if (version) showVersion()
-else if (Deno.args.length === 2) {
-  try {
-    const srcPath = Deno.args[0]
-    const destPath = Deno.args[1]
+else {
+  const srcs = Deno.args.slice(0, -1)
+  const dest = <string>Deno.args.at(-1)
 
-    const srcStats = await Deno.lstat(srcPath)
+  const srcsStat = await srcsExist(srcs)
 
-    if (srcStats.isFile) {
-      const srcText = await Deno.readFile(srcPath)
+  const destStat = await exists(dest)
 
-      const finPath = ['\\', '/'].includes(<string>destPath.at(-1)) ? destPath + srcPath : destPath
+  const srcsBasename = srcs.map(str => basename(str))
 
-      const destFile = await Deno.create(finPath)
+  if (srcs.length === 1) {
+    if (srcsStat[0].isFile) {
+      const srcText = await Deno.readFile(srcs[0])
 
-      await destFile.write(srcText)
-    }
-  } catch (err) {
-    if (err instanceof Deno.errors.NotFound) {
+      const destIsDir = ['\\', '/'].includes(<string>dest.at(-1))
 
-      const file = getFileError(err.message)
+      if (destIsDir) {
+        if (destStat === 'NotFound' || !destStat.isDirectory) showError(CPErrors.NotADirectory, dest)
 
-      showError(CPErrors.NoSuch, file)
-    } else {
-      throw err
+
+      } else {
+        const destFile = await Deno.create(dest)
+        
+        await destFile.write(srcText)
+      }
     }
   }
 
-}
-else {
-  try {
-    const srcs = Deno.args.slice(0, -1)
-    const dest = <string>Deno.args.at(-1)
-
-    const destStat = await Deno.stat(dest)
-
-    if (destStat.isDirectory) {
-      const newFiles = srcs.map(str => join(dest, str))
-      console.log(newFiles, dest)
-    }
-  } catch (err) {
-    if (err instanceof Deno.errors.NotFound) {
-
-      const file = getFileError(err.message)
-
-      showError(CPErrors.NotADirectory, file)
-    } else {
-      throw err
-    }
+  if (destStat === 'NotFound' || !destStat.isDirectory) showError(CPErrors.NotADirectory, dest)
+  else {
+    const newFiles = srcs.map(str => join(dest, str))
+    console.log(newFiles, dest)
   }
 }
--- utils/errors.ts
+++ utils/errors.ts
@@ -1,5 +1,29 @@
-export function getFileError(msg: string) {
-  const init = msg.indexOf('\'')
+import { CPErrors } from '../types/enums.ts'
+import { showError } from './show.ts'
 
-  return msg.slice(init).replaceAll('\'', '')
+type Exists = Deno.FileInfo | 'NotFound'
+
+export async function exists(path: string): Promise<Exists> {
+  try {
+    const pathStats = await Deno.stat(path)
+
+    return pathStats
+
+  } catch (err) {
+    if (err instanceof Deno.errors.NotFound) return 'NotFound'
+    
+    throw err
+  }
+}
+
+export function srcsExist(arr: string[]): Promise<Deno.FileInfo[]> {
+  const arrStats = arr.map(path =>
+    Deno.stat(path).catch(err => {
+      if (err instanceof Deno.errors.NotFound) showError(CPErrors.NoSuch, path)
+
+      throw err
+    })
+  )
+
+  return Promise.all(arrStats)
 }
