feee80a742c3b25f7bdcdd6d8675cef9f9c79c83
TsonkoAlt <matrosmartz@gmal.com>
2022-08-04 21:59:45 -0500

    change information to info in utils/show.ts

--- utils/show.ts
+++ utils/show.ts
@@ -1,35 +1,34 @@
 import { CPErrors } from '../types/enums.ts'
 import { ShowErrors } from '../types/information.ts'
 
-import information from '../data/information.ts'
+import info from '../data/information.ts'
 
 export function showInfo() {
-  console.log(information.description)
+  console.log(info.description)
   console.log('Options:')
-  for (const { names, description } of information.options) {
+
+  for (const { names, description } of info.options) {
     console.log('   ' + names.join(', ') + ':')
     console.log('       ' + description)
   }
+
 }
 
 export function showVersion() {
-  console.log(information.version)
+  console.log(info.version)
 }
 
 export const showErrors: ShowErrors = {
   MissingFiles() {
-    console.error(information.errors[CPErrors.MissingFiles])
-
+    console.error(info.errors[CPErrors.MissingFiles])
     Deno.exit(1)
   },
   NoSuch(str) {
-    console.error(information.errors[CPErrors.NoSuch](str))
-  
+    console.error(info.errors[CPErrors.NoSuch](str))
     Deno.exit(1)
   },
   NotADirectory(str) {
-    console.error(information.errors[CPErrors.NotADirectory](str))
-  
+    console.error(info.errors[CPErrors.NotADirectory](str))
     Deno.exit(1)
   },
 }
