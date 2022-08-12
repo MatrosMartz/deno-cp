24a5daac88cc35ba25501c5cb4b8f7dfa9bb651a
TsonkoAlt <matrosmartz@gmal.com>
2022-08-11 19:21:41 -0500

    fix: error not a dir in dest dir

--- src/copy.ts
+++ src/copy.ts
@@ -10,12 +10,16 @@ import { join, basename } from 'path'
 
 export default async function ({ srcs, dest }: CopyArgs): Promise<void> {
   if (srcs.length === 1) {
-    if (srcs[0].stats.isFile) {
-      const srcText = await Deno.readFile(srcs[0].path)
+    const [src] = srcs
+    if (!src.stats.isDirectory && dest.type === DestType.NoSuchDir)
+      showErrors.NotADirectory(dest.path)
+
+    if (src.stats.isFile) {
+      const srcText = await Deno.readFile(src.path)
 
       const newFilePath =
         dest.type === DestType.Dir ?
-          join(dest.path, basename(srcs[0].path)) :
+          join(dest.path, basename(src.path)) :
           dest.path
 
       try {
